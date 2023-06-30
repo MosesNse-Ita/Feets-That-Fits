cf7114ec4d2fff9b086656c8560bb4e147489a87
Moses Nse-ita <okonitamoseskokoette@gmail.com>
2023-06-30 13:32:26 +0100

    done

--- signout.js
+++ signout.js
@@ -26,14 +26,3 @@ const firebaseConfig = {
         window.location = "index.html";
     }
   });
-
-  //logout function
-
-  let signOutButton = document.getElementById("signout");
-  signOutButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    console.log("logout clicked");
-    auth.signOut();
-    alert("Signed out");
-    window.location = "./index.html";
-  })
